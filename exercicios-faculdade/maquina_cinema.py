0368b8ad44cf7ea57aa31eed8a54b0e625573785
jackrauser4 <juliocesarsouzar122@gmail.com>
2026-09-15 20:20:47 -0300

    maquina_cinema.py

---
+++ exercicios-faculdade/maquina_cinema.py
@@ -0,0 +1,16 @@
+# Bem-vindo á Máquina de Venda Automática de ingresssos de Cinema!
+# Solicita a idade do cliente
+idade = int(input("Por favor, digite sua idade: "))
+#Verificar a idade para sugestão de filmes
+if idade < 12:
+  print("Recomendamos o filme infatil FILME 1.")
+elif 12<= idade <18:
+  print("Recomendamos o filme adolescente FILME 2.")
+else:
+  print("Recomendamos o emocionante FILME 3.")
+# Verifica a disponibilidade de ingressos
+quantidade_ingressos = 10 # Suponha que haja 10 ingressos disponíveis
+if quantidade_ingressos > 0:
+  print("ingressos estão disponìveis. Divirta-se no cinema!")
+else:
+  print("Desculpe, todos os ingressos estão esgotados para hoje.")
